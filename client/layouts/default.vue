d1e9c2bc7049d98ab828b5d7ed278e24a44c5092
B-S-B-Rabbit <mystery.knight113@gmail.com>
2024-04-09 23:43:53 +0300

    feature/HT-27 #comment add navbar-menu layout and respective comps

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,24 +1,10 @@
 <template>
   <div>
     <q-layout>
-      <q-header>
-        <q-toolbar style="background: black; justify-content: space-between">
-          <div
-            class="hamburger"
-            :class="{ active: isActive }"
-            @click="toggleActive"
-          >
-            <span class="line"></span>
-            <span class="line"></span>
-            <span class="line"></span>
-          </div>
-
-          <q-toolbar-title style="display: flex; justify-content: center">
-            <img src="/public/icons/navbar_logo.svg" alt="" />
-          </q-toolbar-title>
-          <q-btn dense flat icon="account_circle" />
-        </q-toolbar>
-      </q-header>
+      <menu-h-t-navbar-mobile
+        logo-src="/icons/navbar_logo.svg"
+        :menu-items="menuItems"
+      />
       <q-page-container>
         <slot></slot>
       </q-page-container>
@@ -27,45 +13,21 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import {
+  mdiAccountCircleOutline,
+  mdiFilm,
+  mdiHome,
+  mdiHeartBox,
+  mdiNewspaper,
+} from "@quasar/extras/mdi-v6";
 
-const isActive = ref(false);
-
-const toggleActive = () => {
-  isActive.value = !isActive.value;
-};
+const menuItems = ref([
+  { label: "Мой профиль", icon: mdiAccountCircleOutline, route: "/profile" },
+  { label: "Главная", icon: mdiHome, route: "/" },
+  { label: "Фильмы", icon: mdiFilm, route: "/films" },
+  { label: "Статьи", icon: mdiNewspaper, route: "/articles" },
+  { label: "Для вас", icon: mdiHeartBox, route: "/for-you" },
+]);
 </script>
 
-<style scoped lang="scss">
-.hamburger {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 30px;
-  height: 28px;
-  cursor: pointer;
-}
-
-.line {
-  width: 100%;
-  height: 3px;
-  background-color: #ffffff;
-  margin: 3px 0;
-  transition:
-    transform 0.3s ease,
-    opacity 0.3s ease;
-}
-
-.active .line:nth-child(1) {
-  transform: translateY(9px) rotate(45deg);
-}
-
-.active .line:nth-child(2) {
-  opacity: 0;
-}
-
-.active .line:nth-child(3) {
-  transform: translateY(-9px) rotate(-45deg);
-}
-</style>
+<style scoped lang="scss"></style>
